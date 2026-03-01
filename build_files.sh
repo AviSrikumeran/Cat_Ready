1caa23f7c3c72c6d509c291d34e991a0aeaa396c
nisvalpatel <nisvalpatel@gmail.com>
2026-03-01 04:13:16 -0600

    chore: update build files

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -e
+python3.11 -m venv .venv
+source .venv/bin/activate
 pip install -r requirements.txt
 # Run migrations if DATABASE_URL is set (e.g. Vercel + ElephantSQL)
 if [ -n "$DATABASE_URL" ]; then
-  python3.11 manage.py migrate --noinput
+  python manage.py migrate --noinput
 fi
-python3.11 manage.py collectstatic --no-input --clear
+python manage.py collectstatic --no-input --clear
